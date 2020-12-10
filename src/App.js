46700c1992fac62c982ef8334d8e5cfd73c1d4ae
Joseph Velliquette <jvelliquette@rokfin.com>
2020-12-10 00:08:43 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.less';
+
+import React from 'react';
+import { Layout } from 'antd';
+import YouTube from 'react-youtube';
+import {playerOpt, SNOWFLAKE_COUNT} from "./util/Constants";
+import Snowfall from 'react-snowfall';
+
+const { Header, Footer, Content } = Layout;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Layout>
+            <Header className='header'>
+
+            </Header>
+            <Content className='content'>
+                <Snowfall
+                    style={{backgroundColor: '#001529'}}
+                    snowflakeCount={SNOWFLAKE_COUNT}
+                />
+            </Content>
+            <Footer className='footer'>
+                <YouTube videoId="Z0ajuTaHBtM" opts={playerOpt} />
+            </Footer>
+        </Layout>
+    );
 }
 
 export default App;
---
+++ src/util/Constants.js
@@ -0,0 +1,9 @@
+export const playerOpt = {
+    height: '1',
+    width: '1',
+    playerVars: {
+        autoplay: 1,
+    },
+};
+
+export const SNOWFLAKE_COUNT = 200;
